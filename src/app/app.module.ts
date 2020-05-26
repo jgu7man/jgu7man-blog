e1d1129efc296591485894eecfff3f3612134658
jgu7man <jgu7man@gmail.com>
2020-05-25 23:12:04 -0500

    project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MaterialModule } from './material.module';
+import { environment } from "src/environments/environment";
+
+import { AngularFireModule, } from "@angular/fire";
+import { AngularFirestoreModule, } from "@angular/fire/firestore";
+import { AngularFireStorageModule, } from "@angular/fire/storage";
+import { AngularFireAuthModule, } from "@angular/fire/auth";
+import { AngularFireMessagingModule, } from '@angular/fire/messaging';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +21,21 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp( environment.firebaseConfig ),
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    AngularFireMessagingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
